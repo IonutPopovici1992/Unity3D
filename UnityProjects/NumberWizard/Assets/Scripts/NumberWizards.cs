fddc172bf894a1c5300618eb45b69ad6fb3ceeaa
John Wayne <ionut_popovici92@yahoo.com>
2017-06-21 18:10:54 +0300

    Added if and else if statements

--- UnityProjects/NumberWizard/Assets/Scripts/NumberWizards.cs
+++ UnityProjects/NumberWizard/Assets/Scripts/NumberWizards.cs
@@ -8,10 +8,22 @@ public class NumberWizards : MonoBehaviour {
 	void Start () {
 		print ("Welcome to Number Wizard!");
 		print ("Pick a number in your head, but don't tell me!");
+
+		print ("Is the number higher or lower than 500?");
+		print ("Up = Higher, Down = Lower, Return = Equal");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			print ("Up arrow pressed!");
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			print ("Down arrow pressed!");
+		} else if (Input.GetKeyDown(KeyCode.Return)) {
+			print ("I won!");
+		}
+
 	}
+
 }
