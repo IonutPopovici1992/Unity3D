d7885d67df62cd9778c5d32951b5c6f760547731
John Wayne <ionutpopovici92@gmail.com>
2017-09-28 16:54:43 +0300

    UnityScripting -> Beginner: - Conventions and Syntax; - IF Statements

---
+++ UnityScripting/Beginner/Conventions and Syntax/Assets/Conventions_and_Syntax.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conventions_and_Syntax : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("This is a test!!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
